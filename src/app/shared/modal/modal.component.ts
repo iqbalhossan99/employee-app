4684716170c56cab915e2b0bf23fa84ee379935d
Md Iqal Hossan <engiqbal1999@gmail.com>
2023-09-29 15:11:38 +0600

    get employees from local storage and display on the table

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { TableComponent } from '../table/table.component';
 
 @Component({
   selector: 'app-modal',
@@ -15,20 +16,22 @@ export class ModalComponent implements OnInit {
     salary: null
   }
 
-  constructor() {
+  constructor( ) {
 
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+   this.getEmployeeDataFromLocalStorage()
+  }
 
   employeeForm = new FormGroup({
-    name: new FormControl(),
-    email: new FormControl(),
-    salary: new FormControl()
+    name: new FormControl(''),
+    email: new FormControl(''),
+    salary: new FormControl(null)
   })
 
   saveEmployeeDataIntoLocalStorage() {
-    const { name, email, salary } = this.employeeForm.value;
+    const { name, email, salary }:any = this.employeeForm.value;
     this.employee = {
       name: name,
       email: email,
@@ -36,7 +39,17 @@ export class ModalComponent implements OnInit {
     }
     this.employeeArr.push(this.employee)
     localStorage.setItem('employees', JSON.stringify(this.employeeArr))
+    this.employeeForm.reset();
+
     console.log("from", this.employeeForm.value, "employee: ", this.employee, "array: ", this.employeeArr);
+  }
+
+  getEmployeeDataFromLocalStorage() {
+    const getEmployees: any = localStorage.getItem('employees')
+    if (getEmployees != null) {
+      this.employeeArr = JSON.parse(getEmployees)
+    }
+    console.log("get employees", this.employeeArr)
   }
 
 }
--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -6,40 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit{
-  employees = [
-    {
-      "id": 1,
-      "name": "Rahim Ahmed",
-      "email": "rahim.ahmed@example.com",
-      "salary": 50000
-    },
-    {
-      "id": 2,
-      "name": "Fatima Khan",
-      "email": "fatima.khan@example.com",
-      "salary": 55000
-    },
-    {
-      "id": 3,
-      "name": "Ayesha Rahman",
-      "email": "ayesha.rahman@example.com",
-      "salary": 48000
-    },
-    {
-      "id": 4,
-      "name": "Kamal Chowdhury",
-      "email": "kamal.chowdhury@example.com",
-      "salary": 60000
-    },
-    {
-      "id": 5,
-      "name": "Sadia Islam",
-      "email": "sadia.islam@example.com",
-      "salary": 52000
-    }
-  ]
+  // employees = [
+  //   {
+  //     "id": 1,
+  //     "name": "Rahim Ahmed",
+  //     "email": "rahim.ahmed@example.com",
+  //     "salary": 50000
+  //   },
+  //   {
+  //     "id": 2,
+  //     "name": "Fatima Khan",
+  //     "email": "fatima.khan@example.com",
+  //     "salary": 55000
+  //   },
+  //   {
+  //     "id": 3,
+  //     "name": "Ayesha Rahman",
+  //     "email": "ayesha.rahman@example.com",
+  //     "salary": 48000
+  //   },
+  //   {
+  //     "id": 4,
+  //     "name": "Kamal Chowdhury",
+  //     "email": "kamal.chowdhury@example.com",
+  //     "salary": 60000
+  //   },
+  //   {
+  //     "id": 5,
+  //     "name": "Sadia Islam",
+  //     "email": "sadia.islam@example.com",
+  //     "salary": 52000
+  //   }
+  // ]
 
+   employees: any;
   constructor() { }
   
-  ngOnInit():void{}
+  ngOnInit(): void{
+    this.getEmployeeDataFromLocalStorage()
+  }
+  
+  getEmployeeDataFromLocalStorage() {
+    const getEmployees: any = localStorage.getItem('employees')
+    if (getEmployees != null) {
+      this.employees = JSON.parse(getEmployees)
+    }
+    console.log("get employees", this.employees)
+  }
 }
